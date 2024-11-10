c29ff13b36fe310effc9f15d8d1ed994714fa792
jyang10086 <janewyang10086@gmail.com>
2024-11-10 00:06:32 -0800

    add event description

--- screens/Events.js
+++ screens/Events.js
@@ -1,50 +1,63 @@
-import React from 'react';
-import { ScrollView, View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import {
+  ScrollView,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { Card } from "@rneui/themed";
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker } from "react-native-maps";
 
 const events = [
   {
     id: 1,
-    title: 'Event1A',
-    dateTime: 'Mon Apr 22 2024 17:30',
-    location: 'Google Building 40, Mountain View, CA',
+    title: "Event1A",
+    dateTime: "Mon Apr 22 2024 17:30",
+    location: "Google Building 40, Mountain View, CA",
     coordinates: { latitude: 37.422, longitude: -122.084 },
+    description: "settle down",
   },
   {
     id: 2,
-    title: 'Test2',
-    dateTime: 'Tue Apr 23 2024 21:21',
-    location: 'San Francisco–Oakland Bay Bridge, San Francisco, CA',
+    title: "Test2",
+    dateTime: "Tue Apr 23 2024 21:21",
+    location: "San Francisco–Oakland Bay Bridge, San Francisco, CA",
     coordinates: { latitude: 37.798, longitude: -122.377 },
+    description: "testtestest",
   },
   // Add more event objects as needed
 ];
 
-export default function EventScreen() {
+export default function EventScreen({ navigation }) {
   return (
     <ScrollView style={styles.container}>
       {events.map((event) => (
-        <Card key={event.id} containerStyle={styles.card}>
-          <View style={styles.mapContainer}>
-            <MapView
-              style={styles.map}
-              initialRegion={{
-                latitude: event.coordinates.latitude,
-                longitude: event.coordinates.longitude,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01,
-              }}
-              scrollEnabled={false}
-              zoomEnabled={false}
-            >
-              <Marker coordinate={event.coordinates} />
-            </MapView>
-          </View>
-          <Text style={styles.title}>{event.title}</Text>
-          <Text style={styles.location}>{`Location: ${event.location}`}</Text>
-          <Text style={styles.dateTime}>{event.dateTime}</Text>
-        </Card>
+        <TouchableOpacity
+          key={event.id}
+          onPress={() => navigation.navigate("EventDetails", { event })}
+        >
+          <Card key={event.id} containerStyle={styles.card}>
+            <View style={styles.mapContainer}>
+              <MapView
+                style={styles.map}
+                initialRegion={{
+                  latitude: event.coordinates.latitude,
+                  longitude: event.coordinates.longitude,
+                  latitudeDelta: 0.01,
+                  longitudeDelta: 0.01,
+                }}
+                scrollEnabled={false}
+                zoomEnabled={false}
+              >
+                <Marker coordinate={event.coordinates} />
+              </MapView>
+            </View>
+            <Text style={styles.title}>{event.title}</Text>
+            <Text style={styles.location}>{`Location: ${event.location}`}</Text>
+            <Text style={styles.dateTime}>{event.dateTime}</Text>
+          </Card>
+        </TouchableOpacity>
       ))}
     </ScrollView>
   );
@@ -56,34 +69,34 @@ const styles = StyleSheet.create({
   },
   card: {
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
     padding: 0,
     marginBottom: 20,
-    backgroundColor: '#2a2a4f',
+    backgroundColor: "#2a2a4f",
   },
   mapContainer: {
-    position: 'relative',
+    position: "relative",
   },
   map: {
     height: 200,
-    width: '100%',
+    width: "100%",
   },
   dateTime: {
     fontSize: 14,
     margin: 10,
     marginTop: 0,
-    color: '#ccc',
+    color: "#ccc",
   },
   title: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     margin: 10,
-    color: '#fff',
+    color: "#fff",
   },
   location: {
     fontSize: 14,
     margin: 10,
     marginTop: 0,
-    color: '#ccc',
+    color: "#ccc",
   },
 });
