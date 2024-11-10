0c0d2d2c5e9c3e5180acbc3cad307a51ad59ee48
yumi-xu <happyleaf90@outlook.com>
2024-11-09 20:54:55 -0800

    Update validateUserInfo.js

--- Components/Me/validateUserInfo.js
+++ Components/Me/validateUserInfo.js
@@ -3,6 +3,9 @@ const MAX_NAME_LENGTH = 20;
 const MAX_ADDRESS_LENGTH = 30;
 const MAX_DESCRIPTION_LENGTH = 100;
 
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const PHONE_REGEX = /^\d{10}$/;
+
 export const validateUserInfo = (userInfo) => {
   const { name, age, sex, avatar, email, phone, address, description } =
     userInfo;
@@ -16,26 +19,45 @@ export const validateUserInfo = (userInfo) => {
   } else if (name.length > MAX_NAME_LENGTH) {
     errors.push(`Name cannot be longer than ${MAX_NAME_LENGTH}!`);
   }
+
   if (!age) {
     errors.push("Age cannot be empty!");
+  } else {
+    const ageValue = +age;
+    if (!ageValue || age <= 0) {
+      errors.push("Age must be a positive number!");
+    }
   }
-  // TODO: sex
-  // TODO: avatar
+
+  if (!sex) {
+    errors.push("Gender cannot be empty!");
+  }
+
+  // TODO: avatar validation if needed
+
   if (!email) {
     errors.push("Email cannot be empty!");
+  } else if (!EMAIL_REGEX.test(email)) {
+    errors.push("Invalid email format!");
   }
+
   if (!phone) {
     errors.push("Phone cannot be empty!");
+  } else if (!PHONE_REGEX.test(phone)) {
+    errors.push("Invalid phone format!");
   }
+
   if (!address) {
     errors.push("Address cannot be empty!");
   } else if (address.length > MAX_ADDRESS_LENGTH) {
     errors.push(`Address cannot be longer than ${MAX_ADDRESS_LENGTH}!`);
   }
+
   if (!description) {
     errors.push("Description cannot be empty!");
   } else if (description.length > MAX_DESCRIPTION_LENGTH) {
     errors.push(`Description cannot be longer than ${MAX_DESCRIPTION_LENGTH}!`);
   }
+
   return errors;
 };
