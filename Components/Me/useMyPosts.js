bfac55dedfec4546765c765c01afe1d8d2656472
yumi-xu <happyleaf90@outlook.com>
2024-11-11 10:45:34 -0800

    fetch my post data from db

--- Components/Me/useMyPosts.js
+++ Components/Me/useMyPosts.js
@@ -1,19 +1,29 @@
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { database } from "../../Firebase/firebaseSetup";
+import { useLoginUserId } from "../UserContext";
+
 export const useMyPosts = () => {
-  // TODO:
-  return [
-    {
-      id: 1,
-      userName: "john_doe",
-      userAvatar: "https://randomuser.me/api/portraits/men/1.jpg",
-      image: "https://placekitten.com/600/400",
-      description: "A beautiful day with this lovely kitten!",
-    },
-    {
-      id: 2,
-      userName: "jane_smith",
-      userAvatar: "https://randomuser.me/api/portraits/women/2.jpg",
-      image: "https://placekitten.com/600/401",
-      description: "Chilling with my furry friend 🐾",
-    },
-  ];
+  const myUserId = useLoginUserId();
+
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const postsQuery = query(
+      collection(database, "Posts"),
+      where("userId", "==", myUserId),
+    );
+
+    const unsubscribePosts = onSnapshot(postsQuery, (querySnapshot) => {
+      const updatedItems = querySnapshot.docs.map((snapDoc) => ({
+        ...snapDoc.data(),
+        id: snapDoc.id,
+      }));
+      setMyPosts(updatedItems);
+    });
+
+    return () => unsubscribePosts();
+  }, []);
+
+  return myPosts;
 };
