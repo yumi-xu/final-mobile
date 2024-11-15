f17a21117aff5d0025de4afeff1045959b45494a
jyang10086 <janewyang10086@gmail.com>
2024-11-14 18:05:26 -0800

    add react dropdown and searchbar

--- screens/Events.js
+++ screens/Events.js
@@ -4,30 +4,27 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  View,
 } from "react-native";
-import { Card } from "@rneui/themed";
+import { Card, SearchBar } from "@rneui/themed";
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { auth, database } from "../Firebase/firebaseSetup";
 import { useNavigation } from "@react-navigation/native";
+import { Dropdown } from "react-native-element-dropdown";
+const sortOptions = [
+  { label: "Name", value: "name" },
+  { label: "Date", value: "date" },
+];
 
-export default function EventScreen({ isFetchAll }) {
+export default function EventScreen() {
   const navigation = useNavigation();
   const [events, setEvents] = useState([]);
-  const fetchEvents = () => {
-    // Define the query based on `isFetchAll`
-    const eventsQuery = isFetchAll
-      ? collection(database, "Events")
-      : query(
-          collection(database, "Events"),
-          where("owner", "==", auth.currentUser.uid)
-        );
-
-    return eventsQuery;
-  };
+  const [search, setSearch] = useState("");
+  const [sortOption, setSortOption] = useState("name");
   useEffect(() => {
     // Subscribe to Events collection
     const unsubscribeEvents = onSnapshot(
-      fetchEvents(isFetchAll),
+      collection(database, "Events"),
       (querySnapshot) => {
         const updatedItems = querySnapshot.docs.map((snapDoc) => ({
           ...snapDoc.data(),
@@ -44,18 +41,44 @@ export default function EventScreen({ isFetchAll }) {
 
   return (
     <ScrollView style={styles.container}>
-      {events.map((event) => (
-        <TouchableOpacity
-          key={event.id}
-          onPress={() => navigation.navigate("EventDetails", { event })}
-        >
-          <Card key={event.id} containerStyle={styles.card}>
-            <Text style={styles.title}>{event.title}</Text>
-            <Text style={styles.location}>{`Location: ${event.location}`}</Text>
-            <Text style={styles.dateTime}>{event.dateTime}</Text>
-          </Card>
-        </TouchableOpacity>
-      ))}
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={setSearch}
+        value={search}
+        containerStyle={styles.searchBar}
+        inputContainerStyle={styles.searchInput}
+      />
+      <View style={styles.sortContainer}>
+        <Text style={styles.sortLabel}>Sort By:</Text>
+        <Dropdown
+          data={sortOptions}
+          labelField="label"
+          valueField="value"
+          placeholder="Sort by"
+          value={sortOption}
+          onChange={(item) => setSortOption(item.value)}
+          style={styles.dropdown}
+          selectedTextStyle={styles.selectedText}
+          containerStyle={styles.dropdownContainer}
+          itemTextStyle={styles.itemText}
+        />
+      </View>
+      <View>
+        {events.map((event) => (
+          <TouchableOpacity
+            key={event.id}
+            onPress={() => navigation.navigate("EventDetails", { event })}
+          >
+            <Card key={event.id} containerStyle={styles.card}>
+              <Text style={styles.title}>{event.title}</Text>
+              <Text
+                style={styles.location}
+              >{`Location: ${event.location}`}</Text>
+              <Text style={styles.dateTime}>{event.dateTime}</Text>
+            </Card>
+          </TouchableOpacity>
+        ))}
+      </View>
     </ScrollView>
   );
 }
@@ -63,6 +86,30 @@ export default function EventScreen({ isFetchAll }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  searchBar: {
+    marginBottom: 10,
+    overflow: "hidden",
+  },
+  searchInput: {
+    backgroundColor: "#f0f0f0",
+  },
+  sortContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  sortLabel: {
+    fontSize: 16,
+    marginRight: 10,
+  },
+  dropdown: {
+    height: 30,
+    borderColor: "#ccc",
+    minWidth: '30%',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
   },
   card: {
     borderRadius: 10,
